8d9eee2ff2a86cffced94a9287a658af6e7a53d8
eduEsron <141090507+eduEsron@users.noreply.github.com>
2024-10-02 07:58:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menu = document.getElementsByClassName("Menu")[0]
 const conteudo = document.getElementById("conteudo")
 const dropdownMenu = document.getElementById("nav")
 const icons = document.getElementsByClassName("icons")[0]
+const returntop = document.getElementById("back")
 conteudo.addEventListener("mouseover", function(){
     dropdownMenu.style.display="block"
 })
@@ -13,4 +14,8 @@ dropdownMenu.addEventListener("mouseout", function(){
 })
 menu.addEventListener("mouseover", function(){
     dropdownMenu.style.display = "none"
+})
+
+returntop.addEventListener("click", function(){
+    location.href="#htmll"
 })
